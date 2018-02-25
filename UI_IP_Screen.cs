a5c8bed436867ea5005cd5494062146555a6fd18
ClaudiaZam <1080357@alumno.um.edu.mx>
2018-02-25 03:35:03 -0600

    Segundo Codigo

---
+++ UI_IP_Screen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AppVac
+{
+	//este se agregó a Login y Register_screen
+	public class UI_IP_Screen : MonoBehaviour
+		{
+	    #region Variables
+	    #endregion
+
+	    #region Main Methods
+	    // Use this for initialization
+	    void Start()
+	    {
+
+	    }
+
+	    // Update is called once per frame
+	    void Update()
+	    {
+
+	    }
+	    #endregion
+
+
+	    #region Helper Methods
+	    #endregion
+		}
+}
