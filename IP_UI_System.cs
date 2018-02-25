0e675db64d360527b3f7f867ab4ef60a967a872a
ClaudiaZam <1080357@alumno.um.edu.mx>
2018-02-25 03:31:37 -0600

    Codigos nuevos

---
+++ IP_UI_System.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AppVac
+{
+	//este se agregó a UI_GRP
+    public class IP_UI_System : MonoBehaviour
+    {
+        #region Variables
+        #endregion
+
+
+        #region Main Methods
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        #endregion
+
+
+        #region Helper Methods
+        #endregion
+
+    }
+}
